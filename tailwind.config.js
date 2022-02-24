36ecf8c0edc64edaba9c07811a1e27dd19716ad8
akir <oakir@ecomundo.eu>
2022-02-24 01:30:52 +0100

    add some templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
